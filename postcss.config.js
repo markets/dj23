7667aecc8bc66468d79a14ac50a5683848ad9b55
Marc Anguera Insa <srmarc.ai@gmail.com>
2025-09-01 16:17:07 +0200

    move css to dist/

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   plugins: [
-    require('autoprefixer')({
-      overrideBrowserslist: [
-        'last 2 versions',
-        '> 1%',
-        'not dead',
-        'not IE 11'
-      ]
-    })
+    require('autoprefixer')
   ]
 }
